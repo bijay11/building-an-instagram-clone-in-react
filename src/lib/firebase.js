baa94c49ab17bfbb9bd46a01cdd201591ba65bdf
Bijay Tamang <lazon11@outlook.com>
2021-10-01 17:19:54 -0400

    Instagram Clone Initial Setup

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const config = {
+  apiKey: "AIzaSyBsbMde9c26mgNX1CBxKDNt6khmSVEGfy0",
+  authDomain: "bijay-instagram.firebaseapp.com",
+  projectId: "bijay-instagram",
+  storageBucket: "bijay-instagram.appspot.com",
+  messagingSenderId: "190607031568",
+  appId: "1:190607031568:web:7bc51daf2133de87b5f995",
+};
+
+const firebase = window.firebase.initializeApp(config);
+const { FieldValue } = window.firebase.firestore;
+
+export { firebase, FieldValue };
