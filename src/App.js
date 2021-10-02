8e072dbf9cc63837e3211b83f71d9f1d42eca96d
Bijay Tamang <lazon11@outlook.com>
2021-10-02 10:18:18 -0400

    added images and router setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 
 const Dashboard = lazy(() => import("./pages/dashboard"));
 const Signup = lazy(() => import("./pages/signup"));
@@ -8,9 +10,17 @@ const NotFound = lazy(() => import("./pages/not-found"));
 
 const App = () => {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <p>This is where our content will be</p>
-    </Suspense>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={Signup} />
+          <Route path={ROUTES.PROFILE} component={Profile} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+          <Route path={ROUTES.NOT_FOUND} component={NotFound} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 };
 
